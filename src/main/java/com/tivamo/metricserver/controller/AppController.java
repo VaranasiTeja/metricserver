08df68a70d0c5920f930027d5089a583b3a5bd02
Teja <teja.varanasi@tivamo.com>
2016-01-28 20:59:07 +0530

    returning response pong using java collections

--- src/main/java/com/tivamo/metricserver/controller/AppController.java
+++ src/main/java/com/tivamo/metricserver/controller/AppController.java
@@ -1,34 +1,17 @@
 package com.tivamo.metricserver.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Collections;
+import java.util.Map;
 
 @RestController
 public class AppController {
 
 	@RequestMapping(value = "/", method = RequestMethod.GET, produces = "application/json")
-	public @ResponseBody Model ping() {
-		return new Model("PONG");
+	public @ResponseBody Map<String, String> ping() {
+		return Collections.singletonMap("value", "PONG");
 	}
-}
-
-class Model {
-	private String value;
-
-	public Model(String value) {
-		super();
-		this.value = value;
-	}
-
-	public String getValue() {
-		return value;
-	}
-
-	public void setValue(String value) {
-		this.value = value;
-	}
-
 }
